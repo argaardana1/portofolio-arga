7cd213367954d663455becadaf625c3e57407233
argaardana1 <remajasehat007@gmail.com>
2026-03-04 22:18:35 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,14 +3,9 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#2563eb",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
